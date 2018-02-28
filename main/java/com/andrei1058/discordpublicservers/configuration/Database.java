067a6b8f7430e7d78b16c5196ddfbb735f7efa8f
andrei1058 <andrew.dascalu@gmail.com>
2018-02-28 16:00:05 +0100

    fixed column type

--- main/java/com/andrei1058/discordpublicservers/configuration/Database.java
+++ main/java/com/andrei1058/discordpublicservers/configuration/Database.java
@@ -61,14 +61,14 @@ public class Database {
         if (!isConnected()) connect();
         try {
             connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS servers (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, server_id BIGINT(200), added_date TIMESTAMP, " +
-                    "server_name VARBINARY(200), server_desc VARBINARY(200), on_users INT(200), tot_users INT(200), bots INT(200), last_bump TIMESTAMP, last_update TIMESTAMP, votes INT(200), " +
-                    "premium INT(1), owner_id BIGINT(200), owner_name VARBINARY(200), invite_link VARCHAR(200), server_icon VARCHAR(200), tags VARCHAR(200), langs VARCHAR(200), display INT(1));");
+                    "server_name BLOB(200), server_desc BLOB(200), on_users INT(200), tot_users INT(200), bots INT(200), last_bump TIMESTAMP, last_update TIMESTAMP, votes INT(200), " +
+                    "premium INT(1), owner_id BIGINT(200), owner_name BLOB(200), invite_link VARCHAR(200), server_icon VARCHAR(200), tags VARCHAR(200), langs VARCHAR(200), display INT(1));");
             connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS banned_servers (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, server_id BIGINT(200), date TIMESTAMP, reason VARCHAR(200));");
-            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS banned_users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id BIGINT(200), date TIMESTAMP, reason VARBINARY(200));");
-            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS feedback (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id BIGINT(200), user_name VARCHAR(200), message VARBINARY(200));");
+            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS banned_users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id BIGINT(200), date TIMESTAMP, reason BLOB(200));");
+            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS feedback (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id BIGINT(200), user_name VARCHAR(200), message BLOB(200));");
             connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS premium_servers (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, server_id BIGINT(200), bought_date TIMESTAMP, expire_date TIMESTAMP);");
             connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS premium_history (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, server_id BIGINT(200), bought_date TIMESTAMP, duration INT(10));");
-            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS reported_servers (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, server_id BIGINT(200), report_date TIMESTAMP, reason VARBINARY(200), reporter BIGINT(200));");
+            connection.createStatement().executeUpdate("CREATE TABLE IF NOT EXISTS reported_servers (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, server_id BIGINT(200), report_date TIMESTAMP, reason BLOB(200), reporter BIGINT(200));");
         } catch (SQLException e) {
             e.printStackTrace();
         }
